38a7dbc0d55f83c7655d74fc01da33703339cb56
Thomas Merkel <tm@core.io>
2013-12-09 18:07:05 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+
+"use strict"
+
+var net = require('net')
+var fs  = require('fs')
+var argv = require('optimist')
+	.options({
+		'source': {
+			alias: 's',
+			demand: true,
+			default: '[::1]:3306',
+		},
+		'destination': {
+			alias: 'd',
+			demand: true,
+		}
+	})
+	.argv
+
+// Make sure args are an array
+argv.source      = (typeof(argv.source)      == 'string') ? [argv.source]      : argv.source
+argv.destination = (typeof(argv.destination) == 'string') ? [argv.destination] : argv.destination
+
+
+function errorHandler(err) {
+	var addr = this.address()
+	var self = this
+
+	if(typeof(addr) == 'string') {
+		if(err.code == 'EADDRINUSE') {
+			var testSocket = net.connect(addr, function(){
+				console.log('unixsocket: Socket is in use ', err)
+				process.exit()
+			})
+			testSocket.on('error', function(err) {
+				if (err.code == 'ECONNREFUSED') {
+					fs.unlink(addr)
+					self.listen(addr)
+				} else {
+					console.log('unixsocket: Socket error ', err)
+					process.exit()
+				}
+			})
+		}
+	} else {
+		console.log('tcpsocket: ', err)
+		process.exit()
+	}
+}
+
+function requestHandler(c) {
+	var d = argv.destination[Math.floor(Math.random()*argv.destination.length)]
+	var host = d.split(':')
+	var port = host.pop()
+
+	var client = net.connect({host: host, port: port}, function() {
+		client.pipe(c)
+		c.pipe(client)
+	})
+
+	client.on('error', function(err){
+		console.log(err, host, port)
+		c.end()
+	})
+
+	c.on('error', function(err){
+		console.log(err)
+		client.end()
+	})
+}
+
+
+var servers = []
+
+for(var i=0; i<argv.source.length; ++i) {
+	var s = argv.source[i]
+	var srv = net.createServer(requestHandler)
+	srv.on('error', errorHandler)
+	if(s.indexOf(":") < 0) {
+		// unix socket
+		srv.listen(s)
+	} else {
+		// tcp socket
+		var host = s.split(':')
+		var port = host.pop()
+		srv.listen(port, host)
+	}
+	servers.push(srv)
+}
+
+process.on('SIGINT', function() {
+	console.log("hallo")
+	for(var i=0; i<servers.length; ++i) {
+		var s = servers[i]
+		if(typeof(s.address()) == 'string') {
+			fs.unlinkSync(s.address())
+		}
+	}
+	process.exit()
+})
